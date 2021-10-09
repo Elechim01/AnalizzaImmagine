2241c64abb09843b93ae0ef15157cbe8deada1fa
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-09 22:24:20 +0200

    Initial Commit

---
+++ AnalizzaImmagine/AnalizzaImmagineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalizzaImmagineApp.swift
+//  AnalizzaImmagine
+//
+//  Created by Michele Manniello on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AnalizzaImmagineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
